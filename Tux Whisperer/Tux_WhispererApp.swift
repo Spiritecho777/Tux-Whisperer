02aacfd1084d6d62756abeda4d372cb7ac615cdd
Rona <rona.chaubell@gmail.com>
2026-07-06 08:31:57 +0200

    Initial Commit

---
+++ Tux Whisperer/Tux_WhispererApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tux_WhispererApp.swift
+//  Tux Whisperer
+//
+//  Created by Ania on 06/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Tux_WhispererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
